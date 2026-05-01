8dca815ea3f97d457fd5dd8bc676d4affae08694
iales <monito.ales@gmail.com>
2026-05-01 20:58:02 -0300

    Mejora: Agregar const en parametros que no cambian y comentario en algortimos de multiplicar

--- operaciones.c
+++ operaciones.c
@@ -2,13 +2,13 @@
 
 /* ── sumar — ya implementada, referencia de estilo ───────────────────────── */
 
-int sumar(int a, int b) {
+int sumar(const int a,const int b) {
     return a + b;
 }
 
 /* ── restar — ya implementada ────────────────────────────────────────────── */
 
-int restar(int a, int b) {
+int restar(const int a, const int b) {
     return a - b;
 }
 
@@ -16,12 +16,12 @@ int restar(int a, int b) {
 
 /* Implementa la función multiplicar, que recibe dos enteros y devuelve su producto. */
 
-int multiplicar(int a, int b) {
+int multiplicar(const int a,const int b) {
     return a * b;
 }
-
+/*Recibe dos parametros de tipo int (numeros enteros) y te retorna el resultado del pruducto*/
 /* ── esPar ────────────────────────────────────────────────────────────────── */
 
-int esPar(int n) {
+int esPar(const int n) {
     return (n % 2) == 0;
 }
